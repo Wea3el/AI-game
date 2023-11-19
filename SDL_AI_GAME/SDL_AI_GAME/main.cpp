aac329bff335267d44f3496d600574c6a2a9e1fd
Wea3el <wesleyz2004@gmail.com>
2023-11-18 23:02:34 -0500

    basic game structure complete

---
+++ SDL_AI_GAME/SDL_AI_GAME/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_AI_GAME
+//
+//  Created by Wesley Zhuang on 11/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
